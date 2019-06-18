b1fe50fae93e2240e57685b0f8ef18e8d0060c0b
xueyu <1375156056@qq.com>
2019-06-18 14:56:20 +0800

    fsweefe

--- src/main/java/com/qf/manager/web/TestDemo1.java
+++ src/main/java/com/qf/manager/web/TestDemo1.java
@@ -2,8 +2,8 @@ package com.qf.manager.web;
 
 public class TestDemo1 {
 
-    public static void mian(String[]args){
+    public static void main(String[]args){
         System.out.println("Hello World!");
-        System.out.println("");
+        System.out.println("xueyu------------------------------------------------------");
     }
 }
